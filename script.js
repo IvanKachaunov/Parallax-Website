32945c5df49560e280bcf51338d4ab7d8c4ea277
Ivan Kachaunov <73343810+IvanKachaunov@users.noreply.github.com>
2021-11-04 00:32:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+.to(".rock1", 3, {y: -300})
+.to(".girl1", 3, {y: -200},'-=3')
+// .fromTo(".bg1", {y: -20}, {y: 0, duration: 3}, '-=3')
+.to(".content", 3, {top: '0%'}, '-=3')
+
+let scene = new ScrollMagic.Scene({
+    triggerElement: "section",
+    duration: "100%",
+    triggerHook: 0,
+})
+ .setTween(timeline)
+ .setPin("section")
+ .addTo(controller);
